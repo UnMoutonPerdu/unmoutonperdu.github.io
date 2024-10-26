100c06d2cf071159e786d72a20b807d9d369a247
UnMoutonPerdu <mtnhugo062@gmail.com>
2024-10-26 01:13:26 -0400

    add text and img

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Blog';
+export const SITE_TITLE = 'Purple Rain';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
-export const SITE_SUBTITLE = '';
+export const SITE_SUBTITLE = 'A simple blog by me';
